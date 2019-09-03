a768bce2a93ff94b94e01978b634a326f4a16f47
Marco Davalos <marcodavalosmantilla@gmail.com>
2019-09-04 00:25:01 +0200

    Variable naming refactor in internal/config/config.go
    Signed-off-by: Marco Davalos <marcodavalosmantilla@gmail.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,20 +6,28 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"sort"
 	"strings"
 )
 
 const (
-	defaultKlingoDir    string      = ".klingo"
-	FullOwnerPermission os.FileMode = 0700
+	defaultKlingoDir       string      = ".klingo"
+	defaultContextsDir     string      = "contexts"
+	DefaultContextFileName string      = "default"
+	defaultConfigFileName  string      = "config"
+	fullOwnerPermission    os.FileMode = 0700
 )
 
 var (
 	config *klingoConfig
 )
 
+func GetKlingoConfig() *klingoConfig {
+	return config
+}
+
 type klingoConfig struct {
-	dir            string
+	dirPath        string
 	contexts       []string
 	currentContext string
 }
@@ -31,13 +39,13 @@ func (c *klingoConfig) CurrentContext() string {
 func (c *klingoConfig) UseContext(context string) error {
 	for _, item := range c.contexts {
 		if item == context {
-			contextsDir := path.Join(c.dir, "contexts")
-			contextPath := path.Join(contextsDir, item)
-			configFileSymLink := path.Join(c.dir, "config")
+			contextsDirPath := path.Join(c.dirPath, defaultContextsDir)
+			contextFilePath := path.Join(contextsDirPath, item)
+			configFileSymLink := path.Join(c.dirPath, defaultConfigFileName)
 			if err := os.Remove(configFileSymLink); err != nil {
 				return err
 			}
-			if err := os.Symlink(contextPath, configFileSymLink); err != nil {
+			if err := os.Symlink(contextFilePath, configFileSymLink); err != nil {
 				return err
 			}
 
@@ -50,10 +58,6 @@ func (c *klingoConfig) UseContext(context string) error {
 	}
 
 	return errors.Errorf("no context exists with the name: %s", context)
-}
-
-func GetKlingoConfig() *klingoConfig {
-	return config
 }
 
 func init() {
@@ -84,34 +88,35 @@ func init() {
 
 // initConfigFileStructure creates the config file structure in the
 // given dir param if needed:
+//
 // .klingo/
 // |__ config (Symbolic link to any contexts/*)
 // |__ contexts/
 // |   |__ default
 func initConfigFileStructure(dir string) error {
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		if err := os.Mkdir(dir, FullOwnerPermission); err != nil {
+		if err := os.Mkdir(dir, fullOwnerPermission); err != nil {
 			return err
 		}
 	} else if err != nil {
 		return err
 	} // TODO: Check permissions of existing directory and warn user if needed
 
-	contextsDir := path.Join(dir, "contexts")
+	contextsDir := path.Join(dir, defaultContextsDir)
 	if _, err := os.Stat(contextsDir); os.IsNotExist(err) {
-		if err := os.Mkdir(contextsDir, FullOwnerPermission); err != nil {
+		if err := os.Mkdir(contextsDir, fullOwnerPermission); err != nil {
 			return err
 		}
 	} else if err != nil {
 		return err
 	}
 
-	defaultContext := path.Join(contextsDir, "default")
-	configFileSymLink := path.Join(dir, "config")
+	defaultContext := path.Join(contextsDir, DefaultContextFileName)
+	configFileSymLink := path.Join(dir, defaultConfigFileName)
 
 	if _, err := os.Stat(defaultContext); os.IsNotExist(err) {
-		comment := []byte("# config file used by default\n")
-		err := ioutil.WriteFile(defaultContext, comment, FullOwnerPermission)
+		comment := []byte("# default config file\n")
+		err := ioutil.WriteFile(defaultContext, comment, fullOwnerPermission)
 		if err != nil {
 			return err
 		}
@@ -146,11 +151,11 @@ func initConfigFileStructure(dir string) error {
 // |   |__ ...
 func loadKlingoConfig(dir string) error {
 	config = &klingoConfig{
-		dir: dir,
+		dirPath: dir,
 	}
 
-	contextsDir := path.Join(dir, "contexts")
-	files, err := ioutil.ReadDir(contextsDir)
+	contextsDirPath := path.Join(dir, defaultContextsDir)
+	files, err := ioutil.ReadDir(contextsDirPath)
 	if err != nil {
 		return err
 	}
@@ -163,12 +168,13 @@ func loadKlingoConfig(dir string) error {
 			)
 		}
 	}
+	sort.Strings(config.contexts)
 
-	destination, err := os.Readlink(path.Join(dir, "config"))
+	currentContextFileName, err := os.Readlink(path.Join(dir, defaultConfigFileName))
 	if err != nil {
 		return err
 	}
 
-	config.currentContext = path.Base(destination)
+	config.currentContext = path.Base(currentContextFileName)
 	return nil
 }
